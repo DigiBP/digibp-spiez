0edbfae11494ac0ecd4bd0b16e28bba1af97d087
PCi3ntisT <vogel.christopher.92@gmail.com>
2019-05-06 09:35:29 +0200

    test

--- src/main/java/ch/fhnw/digibp/ProcessApplication.java
+++ src/main/java/ch/fhnw/digibp/ProcessApplication.java
@@ -12,4 +12,14 @@ public class ProcessApplication {
 		SpringApplication.run(ProcessApplication.class, args);
 	}
 
+ @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*");
+            }
+        };
+    }
+
 }
